bc48db09e3d78676e4df281f42b3e07ec6c2b349
tjncc <55355686+tjncc@users.noreply.github.com>
2020-04-04 20:45:39 +0200

    edited home page for certificates

--- src/components/certificates/CertificatePage.js
+++ src/components/certificates/CertificatePage.js
@@ -10,9 +10,11 @@ class CertificatePage extends React.Component{
 
       render(){
         return(
-          <div>
-              <Button className="btnSertifikat">Trazi sertifikat</Button>
-              <Button className="btnSertifikat">Pregledaj sertifikate</Button>
+          <div className="divCertButtons" style={{top:'0', bottom:'0', left:'0', right:'0', position: 'absolute'}}>
+              <div className="divCertMiddle">
+              <button variant="outline-dark" className="btnSertifikat">Request for a cerificate</button>
+              <a className="aCertificate">Review existing certificates</a>
+              </div>
           </div>
         );
     
