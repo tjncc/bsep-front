7b90f83cf89f1281e25619e2843e5793c172570d
tjncc <55355686+tjncc@users.noreply.github.com>
2020-04-18 21:53:07 +0200

    added field for checking validity

--- src/components/certificates/CertificatePage.js
+++ src/components/certificates/CertificatePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button} from 'react-bootstrap'
+import { Button, Card, Accordion, Form, Dropdown, FormGroup, FormControlLabel } from 'react-bootstrap'
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,19 +12,28 @@ import {
 import axios from 'axios';
 import AllCertificatesPage from '../certificates/AllCertificatesPage';
 import '../../css/certificates/CertificatePage.css'
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 
-
+const sweetAlert = withReactContent(Swal)
 class CertificatePage extends React.Component{
 
     constructor(props){
         super(props);
         this.showAllCertificates = this.showAllCertificates.bind(this);
         this.createCertificate= this.createCertificate.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.checkValidity = this.checkValidity.bind(this);
 
         axios.get("http://localhost:8081/api/certificates/checkroot").then(
           (resp) => { window.location = "http://localhost:3000/createroot" },
           (resp) => {}
         );
+
+        this.state = {
+          status: '',
+          serialnum: '',
+        }
 
       }
 
@@ -36,9 +45,35 @@ class CertificatePage extends React.Component{
         window.location = "http://localhost:3000/createcertificate"
       }
 
+      handleChange(e) {
+        this.setState({ ...this.state, [e.target.name]: e.target.value });
+        console.log(this.state);
+      }
+
+
+      checkValidity(){
+        if(this.state.serialnum != ''){
+        axios.get(`http://localhost:8081/api/certificates/check/${this.state.serialnum}`).then(
+          (resp) => { this.setState({ status: resp.data }) },
+          (resp) => alert('error'),
+        );
+
+        } else {
+          sweetAlert.fire({
+            title: '',
+            text: 'Enter the serial number to check validity of the certificate',
+            type: 'error',
+            icon: 'error',
+            button: true
+          });
+          
+        }
+        
+      }
+
+
       render(){
         return(
-
 
           <div className="divCertButtons" style={{top:'0', bottom:'0', left:'0', right:'0', position: 'absolute'}}>
               <div className="divCertMiddle">
@@ -49,11 +84,28 @@ class CertificatePage extends React.Component{
               <Link className="aCertificate" to="/allcertificates">  Valid certificates </Link>
               <Link className="aRCertificate" to="/revoked">  Revoked certificates </Link>
 
+              </div>
+
+              <div className="divCertDown">
+                <Form>
+                <Form.Group>
+                    <Form.Label style={{color: 'rgb(164, 207, 194)'}}>Check validity of a certificate:</Form.Label>
+                    <Form.Row> 
+                    <Form.Control className="formCntrlNum" style={{width: '78%'}} placeholder="Enter serial number" required name="serialnum" onChange={this.handleChange} />
+                    <Button variant="outline-light" onClick={this.checkValidity}>Check</Button>
+                    </Form.Row>
+                      <Form.Row>
+                        <Form.Label style={{color: 'rgb(164, 207, 194)', margin: '2% 4%'}}>Certificate status: </Form.Label>
+                        <Form.Label></Form.Label>
+                        <Form.Label style={{color: 'rgb(220, 227, 224)', margin: '2% -2.6%' }}>{this.state.status}</Form.Label>
+                      </Form.Row>
+                  </Form.Group>
+                </Form>
 
               </div>
+
+
           </div>
-
-
 
 
         );
